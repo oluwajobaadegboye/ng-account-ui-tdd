eddcb7e2a096db2cd6391e881c9d5baf3e30919e
Oluwajoba Adegboye <oluwajobaadegboye@hotmail.com>
2019-08-19 08:20:55 -0500

    commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './views/header/header/header.component';
+import { FooterComponent } from './views/footer/footer/footer.component';
+import { ContentComponent } from './views/content/content/content.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    ContentComponent
   ],
   imports: [
     BrowserModule,
