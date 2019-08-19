b47d9f0460af8178ffa8cf54ab270c31edbfcbae
= <=>
2019-08-19 15:20:54 -0500

    header setup

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,44 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AccountListComponent } from './views/content/account-list/account-list.component';
+import { AccountCreateComponent } from './views/content/account-create/account-create.component';
+import { ContactListComponent } from './views/content/contact-list/contact-list.component';
+import { ContactCreateComponent } from './views/content/contact-create/contact-create.component';
+import { ActivityListComponent } from './views/content/activity-list/activity-list.component';
+import { ActivityCreateComponent } from './views/content/activity-create/activity-create.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'contacts',
+    pathMatch: 'full'
+  },
+  {
+    path: 'accounts',
+    component: AccountListComponent
+  },
+  {
+    path: 'create-account',
+    component: AccountCreateComponent
+  },
+  {
+    path: 'contacts',
+    component: ContactListComponent
+  },
+  {
+    path: 'create-contact',
+    component: ContactCreateComponent
+  },
+  {
+    path: 'activities',
+    component: ActivityListComponent
+  },
+  {
+    path: 'create-activity',
+    component: ActivityCreateComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,21 +2,44 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { HeaderComponent } from './views/header/header/header.component';
-import { FooterComponent } from './views/footer/footer/footer.component';
-import { ContentComponent } from './views/content/content/content.component';
+import { AppComponent } from './app.component'; 
+import { HeaderComponent } from './views/header/header.component';
+import { FooterComponent } from './views/footer/footer.component';
+import { ContentComponent } from './views/content/content.component';
+import { AccountListComponent } from './views/content/account-list/account-list.component';
+import { AccountCreateComponent } from './views/content/account-create/account-create.component';
+import { ContactListComponent } from './views/content/contact-list/contact-list.component';
+import { ContactCreateComponent } from './views/content/contact-create/contact-create.component';
+import { ActivityListComponent } from './views/content/activity-list/activity-list.component';
+import { ActivityCreateComponent } from './views/content/activity-create/activity-create.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpClientModule } from '@angular/common/http';
+import { MatToolbarModule, MatIconModule, MatSidenavModule, MatListModule, MatButtonModule } from  '@angular/material';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     FooterComponent,
-    ContentComponent
+    ContentComponent,
+    AccountListComponent,
+    AccountCreateComponent,
+    ContactListComponent,
+    ContactCreateComponent,
+    ActivityListComponent,
+    ActivityCreateComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    BrowserAnimationsModule,
+    HttpClientModule,
+    AppRoutingModule,
+    MatToolbarModule,
+    MatSidenavModule,
+    MatListModule,
+    MatButtonModule,
+    MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
